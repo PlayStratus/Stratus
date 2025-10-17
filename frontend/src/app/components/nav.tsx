500620b109585b536d23ee839535199985155e24
John Polasek <polasekj@oregonstate.edu>
2025-10-16 21:24:21 -0700

    Basic pages added with button nav. Will be updated to look better once we have a better idea of what we want the webpage to look like

---
+++ frontend/src/app/components/nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <div className="nav">
+        <a href="/">Home Page</a>
+        <a href="/service">Service Page</a>
+        <a href="/about">About Page</a>
+        <a href="/settings">Settings Page</a>
+    </div>
+  )
+}
---
+++ frontend/src/app/service/page.tsx
@@ -0,0 +1,7 @@
+export default function Service() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl font-bold'>Connect to the server</h1>
+    </div>
+  )
+}
