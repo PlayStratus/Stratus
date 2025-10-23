afa3b036effa1d821aea9d5dec920d9b97f9465a
Nathen dela Torre <nathendelatorre@gmail.com>
2025-10-23 02:38:41 -0700

    Changed service page from client page to server page to address build error

--- frontend/src/app/service/page.tsx
+++ frontend/src/app/service/page.tsx
@@ -1,13 +1,25 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
+type Props = {
+  searchParams: Promise<{ game?: string }>
+}
 
-export default function Service() {
-  const searchParams = useSearchParams();
-  const game = searchParams.get('game');
+export default async function Service({ searchParams }: Props) {
+  const params = await searchParams
+  const game = params.game
+
+  if (!game) {
+    return (
+      <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <p>No service selected</p>
+      </div>
+    )
+  }
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>This is when we would connect you to a our server and you would be able to play {game}</p>
+      <p>
+        This is when we would connect you to our server and you would be able to
+        play {game}
+      </p>
     </div>
-  );
+  )
 }
