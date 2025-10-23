f0b9fecf4522687e86cd086ab336793168c41cf2
Nathen dela Torre <nathendelatorre@gmail.com>
2025-10-23 02:21:42 -0700

    Changes to CI to address issue of wrong pnpm version and simple change to about page (To test CI)

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,19 +2,40 @@ export default function About() {
   return (
     <div className='flex min-h-screen flex-col items-center p-24'>
       <h1 className='text-4xl font-bold m-5'>About us</h1>
-      <p>Our team includes six members: Asher, John, Carol, Izzy, Amin, and Nathen.
-
-I’m Asher, I am a CS major with a background in Linux, and I’m excited to work on the custom OS and Wayland compositor for this project. 
-<br></br><br></br>
-My name is John Polasek, I am pursuing a bachelor’s degree in Computer Science with a certificate in Cybersecurity, with front-end experience and a little backend experience.
-<br></br><br></br>
-I’m Carol, a Graphic Design and CS double-major, and I was invited onto this project as a designer. 
-<br></br><br></br>
-I’m Izzy, an applied CS senior. I have some experience with full stack applications, so I’m looking forward to working on a few components of the project, trying to achieve a seamless experience streaming from the compositor to the web client. 
-<br></br><br></br>
-I’m Amin, a Computer science major with a focus in Cyber Security. I have experience with Systems Engineering and Unreal Engine. I am looking forward to getting this project up and running and doing whatever is possible to reduce latency.
-<br></br><br></br>
-I’m Nathen, a CS major. Through personal projects, I have gained experience with full-stack web development, and through internships, I have also gained experience with audio processing.</p>
+      <p>
+        Our team includes six members: Asher, John, Carol, Izzy, Amin, and
+        Nathen.
+        <br></br>
+        <br></br>
+        I’m Asher, I am a CS major with a background in Linux, and I’m excited
+        to work on the custom OS and Wayland compositor for this project.
+        <br></br>
+        <br></br>
+        My name is John Polasek, I am pursuing a bachelor’s degree in Computer
+        Science with a certificate in Cybersecurity, with front-end experience
+        and a little backend experience.
+        <br></br>
+        <br></br>
+        I’m Carol, a Graphic Design and CS double-major, and I was invited onto
+        this project as a designer.
+        <br></br>
+        <br></br>
+        I’m Izzy, an applied CS senior. I have some experience with full stack
+        applications, so I’m looking forward to working on a few components of
+        the project, trying to achieve a seamless experience streaming from the
+        compositor to the web client.
+        <br></br>
+        <br></br>
+        I’m Amin, a Computer science major with a focus in Cyber Security. I
+        have experience with Systems Engineering and Unreal Engine. I am looking
+        forward to getting this project up and running and doing whatever is
+        possible to reduce latency.
+        <br></br>
+        <br></br>
+        I’m Nathen, a CS major. Through personal projects, I have gained
+        experience with full-stack web development, and through internships, I
+        have also gained experience with audio processing.
+      </p>
     </div>
   )
 }
