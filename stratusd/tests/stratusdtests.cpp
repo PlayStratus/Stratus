8d7eb2f281428c2e005f5a39db946120191fcc35
Amin Hussien <6235052+aminhussien@users.noreply.github.com>
2025-11-09 21:06:15 -0800

    Initial Google Tests integration.

---
+++ stratusd/tests/stratusdtests.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+// Initial Google Tests Setup
+// TODO: Link Status modules to test and finish.
+
+TEST(stratusdTests, SanityCheck) {
+
+}
+
+
+TEST(SideCar, ConnectionWebSocketTest) {
+
+}
+
+TEST(Title, LaunchGameTest) {
+
+}
+
+TEST(Transport, WebTransportInit) {
+
+}
