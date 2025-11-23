c2d5378fe862c285dcf75ce18f3fe29c3c05d7d4
Izzy Lerman <izzylerman14@gmail.com>
2025-11-23 12:33:52 -0800

    Revert "encode_video function implemented" - should be pull request
    This reverts commit 315fa4ebe3d60e30cc5dfdd6975662edb4aed2c4.

--- stratusd/Encode/include/Encode.h
+++ stratusd/Encode/include/Encode.h
@@ -1,11 +1,7 @@
 #ifndef ENCODE_H
 #define ENCODE_H
 
-
-
-void create_sample_video(const char *filename, int width, int height, int num_frames);
-void encode_argb_video(const char *input_file, const char *output_file,
-                       int width, int height, int num_frames);
-
+void test_ffmpeg();
+void init_encoder();
 
 #endif
