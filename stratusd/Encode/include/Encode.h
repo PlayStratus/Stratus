5ede09e7ff164baced84bd9e287d993ae6cb0b67
Izzy Lerman <izzylerman14@gmail.com>
2025-11-23 12:23:30 -0800

    encode_video function implemented

--- stratusd/Encode/include/Encode.h
+++ stratusd/Encode/include/Encode.h
@@ -1,7 +1,11 @@
 #ifndef ENCODE_H
 #define ENCODE_H
 
-void test_ffmpeg();
-void init_encoder();
+
+
+void create_sample_video(const char *filename, int width, int height, int num_frames);
+void encode_argb_video(const char *input_file, const char *output_file,
+                       int width, int height, int num_frames);
+
 
 #endif
