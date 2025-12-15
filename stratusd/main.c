cd8663b4094b1b7802741e188b0b6e487f04e6b3
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2025-12-15 13:27:22 -0800

    Add basic capture_test function
    Also rewrote stratusd's main() to call test code for different modules
    according to its arguments.

--- stratusd/main.c
+++ stratusd/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "Capture.h"
 #include "Encode.h"
 
-int main()
-{
-    printf("Stratus Service: Hello World \n");
-    test_ffmpeg();
-    init_encoder();
+int main(int argc, char *argv[]) {
+    if (argc >= 2 && !strcmp(argv[1], "capture")) {
+        capture_test();
+    } else if (argc >= 2 && !strcmp(argv[1], "encode")) {
+        test_ffmpeg();
+        init_encoder();
+    } else {
+        printf("Usage: %s [capture|encode]\n", argv[0]);
+    }
 
     return 0;
 }
