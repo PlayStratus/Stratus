eb47103fcf263801c966d9ccd397be8667e84a48
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2025-12-17 13:05:16 -0800

    Remove libwayland dependency

--- stratusd/Capture/src/Capture.c
+++ stratusd/Capture/src/Capture.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <wayland-server.h>
 
 int capture_test() {
-
-    struct wl_display *server_display = wl_display_create();
-    if (!server_display) {
-        fprintf(stderr, "Unable to create Wayland display.\n");
-        return -1;
-    }
-
-    if (wl_display_add_socket(server_display, "stratus")) {
-        fprintf(stderr, "Unable to add socket to Wayland display.\n");
-        return -1;
-    }
-
-    printf("Running wayland server on $XDG_RUNTIME_DIR/stratus socket\n");
-    wl_display_run(server_display);
-
-    wl_display_destroy(server_display);
+    printf("capture_test\n");
     return 0;
 }
