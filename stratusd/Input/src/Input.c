1583e6d66c57628ddd8dca9b8d763fa8d2b6ef8e
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2026-02-05 13:38:29 -0800

    Implement basic gamepad and console interface
    The gamepad console interface is very primitive, but enables
    SuperTuxKart to be played. See Input.c for more details. Without
    additional configuration, the Input module will need root privileges to
    access /dev/uinput on most systems.

--- stratusd/Input/src/Input.c
+++ stratusd/Input/src/Input.c
@@ -1,7 +1,74 @@
-#include <libevdev/libevdev.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "Input.h"
+#include "gamepad.h"
+
+/*
+ * Run a very primitive gamepad interface, optimized for SuperTuxKart
+ *
+ * Supported keys:
+ *  - q: quit
+ *  - w: Y (accelerate)
+ *  - a: Left joystick left (turn left)
+ *  - s: X (brake / reverse)
+ *  - d: Left joystick right (turn right)
+ *  - <ENTER>: A (select / look back)
+ *
+ * Note that only one key can be active at a time, and that each key remains
+ * pressed until a new key is pressed. Use an unbound key (such as <SPACE>) to
+ * force the previous key to be released. These controls are far from ideal, but
+ * technically allow the game to be played and demonstrate the potential of a
+ * libevdev-based virtual gamepad device.
+ */
+void gamepad_stk_console(struct gamepad *gamepad) {
+    char c;
+    struct gamepad_state state;
+
+    if (!isatty(0)) {
+        fprintf(stderr, "Not running SuperTuxKart controller console because stdin isn't a tty\n");
+        return;
+    }
+
+    printf("Running SuperTuxKart controller console...\n");
+
+    system("/bin/stty cbreak");
+    while ((c = getchar()) != 'q') {
+        system ("/bin/stty cooked");
+
+        memset(&state, 0x00, sizeof(struct gamepad_state));
+
+        if (c == '\n')
+            state.buttons[GAMEPAD_A] = 1;
+        else if (c == 'w')
+            state.buttons[GAMEPAD_Y] = 1;
+        else if (c == 's')
+            state.buttons[GAMEPAD_X] = 1;
+        else if (c == 'a')
+            state.axes[GAMEPAD_LEFT_JOYSTICK_X] = -32;
+        else if (c == 'd')
+            state.axes[GAMEPAD_LEFT_JOYSTICK_X] = +32;
+
+        gamepad_update(gamepad, &state);
+
+        system ("/bin/stty cbreak");
+    }
+}
 
 int input_test() {
+    struct gamepad *gamepad;
+
+    gamepad = gamepad_init("stratus");
+    if (gamepad == NULL)
+        return 1;
+
+    usleep(10000);  // Wait 10ms for device to be detected
+
+    gamepad_stk_console(gamepad);
+
+    gamepad_destroy(gamepad);
+
     return 0;
 }
---
+++ stratusd/Input/src/gamepad.c
@@ -0,0 +1,162 @@
+/*
+ * Virtual gamepad device
+ *
+ * Implements a virtual gamepad device using libevdev. See [1] for more
+ * information on gamepad support in Linux.
+ *
+ * [1]: https://www.kernel.org/doc/html/latest/input/gamepad.html
+ */
+
+#include <libevdev/libevdev-uinput.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "gamepad.h"
+
+/*
+ * Linux input event codes for gamepad buttons
+ */
+const unsigned int gamepad_button_codes[GAMEPAD_BUTTON_COUNT] = {
+    [GAMEPAD_A]              = BTN_A,
+    [GAMEPAD_B]              = BTN_B,
+    [GAMEPAD_X]              = BTN_X,
+    [GAMEPAD_Y]              = BTN_Y,
+    [GAMEPAD_LEFT_SHOULDER]  = BTN_TL,
+    [GAMEPAD_RIGHT_SHOULDER] = BTN_TR,
+    [GAMEPAD_LEFT_TRIGGER]   = BTN_TL2,
+    [GAMEPAD_RIGHT_TRIGGER]  = BTN_TR2,
+    [GAMEPAD_SELECT]         = BTN_SELECT,
+    [GAMEPAD_START]          = BTN_START,
+    [GAMEPAD_LEFT_THUMB]     = BTN_THUMBL,
+    [GAMEPAD_RIGHT_THUMB]    = BTN_THUMBR,
+    [GAMEPAD_UP]             = BTN_DPAD_UP,
+    [GAMEPAD_DOWN]           = BTN_DPAD_DOWN,
+    [GAMEPAD_LEFT]           = BTN_DPAD_LEFT,
+    [GAMEPAD_RIGHT]          = BTN_DPAD_RIGHT,
+    [GAMEPAD_MODE]           = BTN_MODE,
+};
+
+/*
+ * Linux input event codes for gamepad axes
+ */
+const unsigned int gamepad_axis_codes[GAMEPAD_AXIS_COUNT] = {
+    [GAMEPAD_LEFT_JOYSTICK_X]  = ABS_X,
+    [GAMEPAD_LEFT_JOYSTICK_Y]  = ABS_Y,
+    [GAMEPAD_RIGHT_JOYSTICK_X] = ABS_RX,
+    [GAMEPAD_RIGHT_JOYSTICK_Y] = ABS_RY,
+};
+
+/*
+ * Configuration for each gamepad axis
+ *
+ * TODO: Update config to match the configuration of a typical physical gamepad.
+ */
+const struct input_absinfo absinfo = {
+    0,      // value
+    -127,   // minimum
+    127,    // maximum
+    0,      // fuzz (controls filter)
+    0,      // flat (controls dead-zone)
+    1,      // resolution
+};
+
+/*
+ * Create a virtual gamepad device
+ *
+ * Returns the libevdev_uinput struct on success and NULL on failure.
+ */
+struct gamepad *gamepad_init(char *name) {
+    int i;
+    struct libevdev *dev;
+    struct gamepad *gamepad;
+
+    // Create gamepad struct
+    gamepad = malloc(sizeof(struct gamepad));
+    if (gamepad == NULL)
+        goto err_malloc;
+    memset(gamepad, 0x00, sizeof(struct gamepad));
+
+    // Initialize libevdev device
+    dev = libevdev_new();
+    libevdev_set_name(dev, name);
+
+    // Configure supported uinput events
+    for (i = 0; i < GAMEPAD_BUTTON_COUNT; i++) {
+        if (libevdev_enable_event_code(dev, EV_KEY, gamepad_button_codes[i],
+                                       NULL) < 0)
+            goto err_event_code;
+    }
+    for (i = 0; i < GAMEPAD_AXIS_COUNT; i++) {
+        if (libevdev_enable_event_code(dev, EV_ABS, gamepad_axis_codes[i],
+                                       &absinfo) < 0)
+            goto err_event_code;
+    }
+
+    // Register gamepad device
+    libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED,
+                                       &gamepad->uidev);
+    if (gamepad->uidev == NULL)
+        goto err_create;
+
+    libevdev_free(dev);
+    return gamepad;
+
+err_create:
+err_event_code:
+    libevdev_free(dev);
+    free(gamepad);
+err_malloc:
+    return NULL;
+}
+
+/*
+ * Update the state of a virtual gamepad device
+ *
+ * Sends uinput events for any gamepad buttons/axes that are active or have
+ * changed since the last update.
+ *
+ * Returns 0 on success and -1 on failure.
+ */
+int gamepad_update(struct gamepad *gamepad, struct gamepad_state *new_state) {
+    bool changed;
+    int i, err;
+
+    changed = false;
+    err = 0;
+
+    for (i = 0; i < GAMEPAD_BUTTON_COUNT; i++) {
+        if (new_state->buttons[i] || gamepad->state.buttons[i]) {
+            // Button is pressed, or was just unpressed
+            changed = true;
+            err += libevdev_uinput_write_event(gamepad->uidev, EV_KEY,
+                                               gamepad_button_codes[i],
+                                               new_state->buttons[i] ? 1 : 0);
+            gamepad->state.buttons[i] = new_state->buttons[i];
+        }
+    }
+
+    for (i = 0; i < GAMEPAD_AXIS_COUNT; i++) {
+        if (new_state->axes[i] != 0 || gamepad->state.axes[i] != 0) {
+            // Axis is off-center, or was just re-centered
+            changed = true;
+            err += libevdev_uinput_write_event(gamepad->uidev, EV_ABS,
+                                               gamepad_axis_codes[i],
+                                               new_state->axes[i]);
+            gamepad->state.axes[i] = new_state->axes[i];
+        }
+    }
+
+    if (changed)
+        err += libevdev_uinput_write_event(gamepad->uidev, EV_SYN, SYN_REPORT,
+                                           0);
+
+    return err == 0 ? 0 : -1;
+}
+
+/*
+ * Destroy a virtual gamepad device and free its resources
+ */
+void gamepad_destroy(struct gamepad *gamepad) {
+    libevdev_uinput_destroy(gamepad->uidev);
+    free(gamepad);
+}
