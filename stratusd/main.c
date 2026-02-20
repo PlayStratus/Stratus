f78967dec684923635ca7eecbe08c12cd755afd3
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2026-02-20 13:37:15 -0800

    Add --version argument to stratusd

--- stratusd/main.c
+++ stratusd/main.c
@@ -1,15 +1,20 @@
 #include <stdbool.h>
 
 #include "SideCar.h"
+#include "version.h"
 
 #define MAX_ENCODE_OUTPUT_NAME 64
 
 int main(int argc, char *argv[]) {
     bool daemon = false;
-    if (argc >= 2 && !strcmp(argv[1], "--daemon")) {
+
+    if (argc >= 2 && !strcmp(argv[1], "--version")) {
+        printf("stratusd %s\n", STRATUSD_VERSION);
+        return 0;
+    } else if (argc >= 2 && !strcmp(argv[1], "--daemon")) {
         daemon = true;
     } else if (argc >= 2) {
-        printf("Usage: %s\n", argv[0]);
+        printf("Usage: %s [--version] [--daemon]\n", argv[0]);
         return -1;
     }
 
