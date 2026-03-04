b2b16d37cc55ac05eebbb2d0d893ba45409c2b0f
John Polasek <polasekj@oregonstate.edu>
2026-03-03 21:32:09 -0800

    Forgot to save before last commit

--- backend/socket/send.ts
+++ backend/socket/send.ts
@@ -2,21 +2,21 @@ import { WebSocket } from "ws"
 import { v4 as uuidv4 } from "uuid"
 import { findNodeByGame } from "./node"
 
-export function startGameSession(gameId: string, userId: string, userName: string) {
-  const ws = findNodeByGame(gameId)
+export function startGameSession(gameId: string, userId: string, userName: string) {        //pass in value from request
+  const ws = findNodeByGame(gameId)                                                         //find game
   if (!ws) {
     console.error("No node available for game:", gameId)
     return
   }
-  const startMessage = {
+  const startMessage = {                                                                    //build message
     type: "start",
     request_id: uuidv4(),
     timestamp: new Date().toISOString(),
     payload: {
       session_id: uuidv4(),
       game_id: gameId,
-      width: "1920",
-      height: "1080",
+      width: "temp",
+      height: "temp",
       session_token: "temp",
       user_id: userId,
       user_name: userName,
