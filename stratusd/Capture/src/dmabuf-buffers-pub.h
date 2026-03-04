4bd442e496246a80b42b066220bd4c2766fa1d3e
Izzy Lerman <izzylerman14@gmail.com>
2026-03-04 10:37:20 -0800

    Remove renamed files

--- stratusd/Capture/src/dmabuf-buffers-pub.h
+++
@@ -1,8 +0,0 @@
-#ifndef CAPTURE_DMABUF_BUFFERS_PUB_H
-#define CAPTURE_DMABUF_BUFFERS_PUB_H
-
-#include "capture-priv.h"
-
-extern const struct message_handler dmabuf_buffers_message_handlers[];
-
-#endif
