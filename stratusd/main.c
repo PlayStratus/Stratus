c36031edb30f778324f3f0fa97b8bacc9cde12c8
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2026-04-07 09:44:26 -0700

    Remove --daemon option from stratusd
    This option is not needed after the main SideCar event loop was
    implemented in #91.

--- stratusd/main.c
+++ stratusd/main.c
@@ -7,20 +7,13 @@
 #include "version.h"
 
 int main(int argc, char *argv[]) {
-    bool daemon = false;
-
     if (argc >= 2 && !strcmp(argv[1], "--version")) {
         printf("stratusd %s\n", STRATUSD_VERSION);
         return 0;
-    } else if (argc >= 2 && !strcmp(argv[1], "--daemon")) {
-        daemon = true;
     } else if (argc >= 2) {
-        printf("Usage: %s [--version] [--daemon]\n", argv[0]);
+        printf("Usage: %s [--version]\n", argv[0]);
         return -1;
     }
 
-    sidecar_main();
-
-    while (daemon)
-        sleep(1);
+    return sidecar_main();
 }
