004f0b2a3d988cb6668bd272d0684b63ec9e5dfb
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2026-05-02 11:11:16 -0700

    Fix bug with Wine controller Y-axes
    The dotnet45 library must now be installed via stratus-launcher --init
    before any games are run.

--- games/README.md
+++ games/README.md
@@ -9,12 +9,14 @@ environments for each game instance.
 ## Development Setup
 
 1.  Install the required dependencies: [`appimagetool`][appimagetool-releases],
-    `bubblewrap`, `wine`, and `wine-mono`
+    `bubblewrap`, `wine`, `wine-mono`, and `winetricks`
 
 2.  Copy or link the `stratus-launcher` script to a directory in `$PATH`:
     `ln -srf ./stratus-launcher.sh ~/bin/stratus-launcher`
 
-3.  Build and run a game: `./build-supertuxkart.sh && ./build/supertuxkart`
+3.  Initialize the wine prefix: `stratus-launcher --init`
+
+4.  Build and run a game: `./build-supertuxkart.sh && ./build/supertuxkart`
 
 
 [appimagetool-releases]: https://github.com/AppImage/appimagetool/releases
--- games/stratus-launcher.sh
+++ games/stratus-launcher.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/sh
 
-# Usage: stratus-launcher wine <exe path relative to ~/.wine/drive_c> <args>...
+# Usage:
+#   stratus-launcher --init
+#   stratus-launcher wine <exe path relative to $APPDIR> <args>...
 
-# This launcher script creates a bubblewrap "sandbox" that overlays $APPDIR/c
-# and $APPDIR/home into ~/.wine and ensures that all writes are non-persistant.
-# It must be called from the AppRun script of an AppImage.
+# This launcher script creates a bubblewrap "sandbox" that ensures that all
+# writes are non-persistent. It must be called from the AppRun script of an
+# AppImage.
 
+set -e
+
+# Initialize wine and install required libraries
+if [ "$1" = '--init' ]; then
+    wineboot --init
+    winetricks -q dotnet45
+    winetricks -q vcrun2015
+    exit 0
+fi
+
+# Check preconditions
 if [ -z "$APPDIR" ]; then
     echo 'Error: Not called from an AppImage'
     exit 1
 fi
-
-# Ensure wine has been initialized
-wineboot --init
+if [ ! -d ~/.wine ]; then
+    echo "Error: ~/.wine does not exist, please run $(basename $0) --init"
+    exit 1
+fi
 
 bwrap_args=()
 
